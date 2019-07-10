82e909e07b496cbff780a5b84311155c1cec6e81
Neoncaste <neonnetwork1337@gmail.com>
2019-07-10 02:10:25 +0100

    Defines

--- common/defines/00_aeternum.lua
+++ common/defines/00_aeternum.lua
@@ -35,6 +35,7 @@ NDefines.NDiplomacy.NUM_OF_GREAT_POWERS = 5 --Decreased from 8
 NDefines.NDiplomacy.LEAVING_GREAT_POWER_YEARS = 10 --Increased from 5
 NDefines.NDiplomacy.GREAT_POWER_SUBJECT_CONTRIBUTION = 0.8 --Amount your subjects contribute to your great power rank. Increased from 0.5.
 NDefines.NDiplomacy.PEACE_COST_GOLD_MAX = 8 --Increased from 5
+
 --NCountry
 
 NDefines.NCountry.MONARCH_MAX_SKILL = 8 --Highest amount of mana a leader can have. Increased from 6.
@@ -43,7 +44,11 @@ NDefines.NCountry.ABDICATE_RULING_LENGTH_THRESHOLD = 10 --Decreased from 20
 NDefines.NCountry.ABDICATE_LEGITIMACY_HIT = -50 --Increased from -20
 NDefines.NCountry.ABDICATE_PRESTIGE_HIT = -20 --Decreased from -50
 NDefines.NCountry.REVOLT_SIZE_BASE = 5 --Increased from 4.
-
+NDefines.NCountry.WAREXHAUSTION_REDUCTION = 2.5 --Increased from 2 due to higher W.E cap
+NDefines.NCountry.STATE_MAINTENANCE_CULTURE_FACTOR = 0.30,		-- Increased from 0.25
+NDefines.NCountry.ADVISOR_COST_INCREASE_PER_YEAR = 0.006 --Increased from 0.005
+NDefines.NCountry.PROMOTE_COST_MONTHS_ADVISOR_SALARY = 70.0 --Increased from 60.0
+NDefines.NCountry.MERCHANT_REPUBLIC_SIZE_LIMIT = 22 --Increased from 20
 --Changes to estates. These should make estate decisions more meaningful and the effects more important.
 NDefines.NCountry.ESTATE_LOYALTY_DECAY_BASE_MIN = 0.5		-- Decreased from 1.0 Loyalty change per year at middle level. 
 NDefines.NCountry.ESTATE_LOYALTY_DECAY_BASE_MAX = 1.0		-- Decreased from 2.0 Loyalty change per year at most extreme levels.
@@ -61,10 +66,34 @@ NDefines.NCountry.PS_NAVAL_BARRAGE = 25 --Decreased from 50
 NDefines.NCountry.PS_REPLACE_RIVAL = 50 --Decreased from 100
 NDefines.NCountry.PS_MOVE_CAPITAL = 150 --Decreased from 200
 NDefines.NCountry.PS_REMOVE_ACCEPTED_CULTURE_UNREST_DURATION = 20 --Increased from 5
+NDefines.NCountry.PS_REMOVE_ACCEPTED_CULTURE = 25 --Cost to remove accepted culture. Increased from 10
 NDefines.NCountry.OVEREXTENSION_FACTOR = 0.8 --Decreased from 1.0, overextension is less potent. Fewer rebels, more dangerous when they do spawn.
 NDefines.NCountry.OVEREXTENSION_OVERSEAS_FACTOR = 0.4 --Decreased fron 0.5, Fewer rebels, more dangerous when they do spawn.
-NDefines.NCountry.DEVELOPMENT_CAP_BASE = 20	-- Increased from 10AI will not develop provinces that have more development than this or DEVELOPMENT_CAP_MULT*original development (whichever is bigger)
+NDefines.NCountry.DEVELOPMENT_CAP_BASE = 20	-- Increased from 10. AI will not develop provinces that have more development than this or DEVELOPMENT_CAP_MULT*original development (whichever is bigger)
 NDefines.NCountry.DEVELOPMENT_CAP_MULT = 3 -- Increased from 2
+
+--Changes to vassals
+NDefines.NCountry.LIBERTY_DESIRE_HISTORICAL_RIVAL = 40 --Decreased from 50.
+NDefines.NCountry.LIBERTY_DESIRE_GREAT_POWER_VASSAL = 55 --Increased from 50.
+NDefines.NCountry.LOST_INDEPENDANCE_WAR_LIBERTY_DESIRE = -65 --Increased from -50.
+NDefines.NCountry.MAX_CROWN_COLONIES = 6 --Increased from 4.
+NDefines.NCountry.CAN_CONVERT_TERRITORY_CULTURE = 0 --No longer able to culture convert territory provinces.
+
+
+--NEconomy
+-- Changes to the economy
+NDefines.NEconomy.DEBASE_ADDED_CORRUPTION = 2.5 --Increased from 2.
+NDefines.NEconomy.AUTONOMY_AT_DIPLO_ANNEX = 30 --Decreased from 60.
+NDefines.NEconomy.AUTONOMY_AT_CONQUEST = 75 --Increased from 50.
+NDefines.NEconomy.AUTONOMY_AT_CONQUEST_CORE = 10 --Increased from 0.
+NDefines.NEconomy.BANKRUPTCY_PROVINCE_DEVASTATION_GAIN = 15 --Increased from 10.
+NDefines.NEconomy.MAX_PROVINCE_SELL_PRICE = 200 --Increased from 100.
+NDefines.NEconomy.MERCHANT_SPEED = 15.0 --Decreased from 20.
+NDefines.NEconomy.EMBARGO_BASE_EFFICIENCY = 0.33 --Decreased from 0.50.
+NDefines.NEconomy.EMBARGO_MERCANTILISM_EFFICIENCY = 33 --Decrased from 0.50.
+NDefines.NEconomy.REGAIN_COST_BEFORE_PROGRESS = 0.15 --Increased from 0.10.
+NDefines.NEconomy.COT_DOWNGRADE_PRESTIGE_COST = -20 --Increased from -10.
+
 
 --NMilitary
 --Changes to combat
@@ -72,7 +101,15 @@ NDefines.NMilitary.LOOT_DEVASTATION_IMPACT = 15 --Increased from 10
 NDefines.NMilitary.SCORCHED_DEVASTATION_IMPACT = 15 --Increased from 5
 NDefines.NMilitary.SEA_LANDING_PENALTY = -3 --Increased from -2
 NDefines.NMilitary.REBEL_LEADER_POWER = 25 --Increased from 20. Fewer rebels, more dangerous when they do spawn.
-NDefines.NMilitary.SIEGE_ATTRITION = 0.95 -- Decreased from 100
+NDefines.NMilitary.SIEGE_ATTRITION = 0.80 -- Decreased from 100
+NDefines.NMilitary.SIEGE_DISEASE_IMPACT = 0.08 --Increased from 0.05
+NDefines.NMilitary.ASSAULT_DEFENDER_LOSS = 0.12 --Increased from 0.05
+NDefines.NMilitary.CAVALRY_SPEED = 0.8 --Increased from 0.7
+NDefines.NMilitary.LOOTED_MAX = 6 --Increased from 5.
+NDefines.NMilitary.DEFAULT_WARGOAL_WARSCORE_BONUS	= 8 --Increased from 5.
+NDefines.NMilitary.COMBAT_DICE_SIDE = 6 --Reduced from 10
+NDefines.NMilitary.FORT_FLIPPING_TIME = 60 --Increased from 30.
+
 
 --NAI
 --Changes to AI behavior. Increased overall agressiveness and geo-political interaction.
